358a665b925ea05ba571e1028f525f5b8be70e48
Ekram Hosen <ekramhosen329@gmail.com>
2023-07-25 22:10:29 +0600

    next route group

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LandingPage = () => {
+    return (
+        <div>
+            Landing Page (Unprocted);
+        </div>
+    );
+};
+
+export default LandingPage;
