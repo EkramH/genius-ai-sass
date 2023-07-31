f552588dc0ff8eb4c99229432b61a31c20589476
Ekram Hosen <ekramhosen329@gmail.com>
2023-07-31 22:14:47 +0600

    add clerk auth

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,9 +1,18 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 import React from 'react';
 
 const LandingPage = () => {
     return (
         <div>
             Landing Page (Unprocted);
+            <div>
+                <Link href="/sign-in">
+                    <Button>
+                        Login
+                    </Button>
+                </Link>
+            </div>
         </div>
     );
 };
