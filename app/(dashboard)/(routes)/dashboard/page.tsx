61aba9fadf3b543f293b47fd917ff407ac817fc8
Ekram Hosen <ekramhosen329@gmail.com>
2023-08-02 22:14:24 +0600

    login and sign out btn

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,9 +1,11 @@
+import { UserButton } from '@clerk/nextjs';
 import React from 'react';
 
 const DashboardPage = () => {
     return (
         <div>
             <h2>DashboardPage (protected)</h2>
+            <UserButton afterSignOutUrl='/' />
         </div>
     );
 };
