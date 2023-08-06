503b4e78b95e0e49885e20f857b692590a499934
Ekram Hosen <ekramhosen329@gmail.com>
2023-08-06 21:17:07 +0600

    dashboard ui

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -3,6 +3,7 @@
 import { Montserrat } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 import { cn } from "@/lib/utils";
 import { LayoutDashboard, MessageSquare, ImageIcon, VideoIcon, Music, Code, Settings  } from "lucide-react";
@@ -74,7 +75,7 @@ const Sidebar = () => {
                         <Link 
                             href={route.href} 
                             key={route.href}
-                            className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition"
+                            className={cn("text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition", pathName === route.href ? "text-white bg-white/10" : "text-zinc-400" )}
                         >
                             <div className="flex items-center flex-1">
                                 <route.icon className={cn("h-5 w-5 mr-3", route.color)}/>
